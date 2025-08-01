1156894a257f1f803b2f6471c16b99128f71f4bd
FarasSchmidt <faras.a.schmidt@gmail.com>
2025-07-31 22:53:07 -0300

    arreglos css y template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { CharactersComponent } from './pages/characters/characters.component';
 import { CharacterDetailComponent } from './pages/character-detail/character-detail.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'characters', pathMatch: 'full' },
   { path: 'characters', component: CharactersComponent },
   { path: 'characters/:id', component: CharacterDetailComponent },
+  { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '/404' },
 ];
