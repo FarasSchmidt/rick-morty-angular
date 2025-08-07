998c2b065a47820d04a2696fa0051f07a8e8cc6c
FarasSchmidt <faras.a.schmidt@gmail.com>
2025-08-07 00:18:40 -0300

    remuevo ssr para el firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideServerRendering } from '@angular/ssr';
 
 export const config: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideServerRendering()
   ]
 };
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import { CharactersComponent } from './pages/characters/characters.component';
-import { CharacterDetailComponent } from './pages/character-detail/character-detail.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'characters', pathMatch: 'full' },
   { path: 'characters', component: CharactersComponent },
-  { path: 'characters/:id', component: CharacterDetailComponent },
+
+{
+  path: 'characters/:id',
+  loadComponent: () => import('./pages/character-detail/character-detail.component').then(m => m.CharacterDetailComponent),
+  data: {
+    renderMode: 'client'
+  }
+},
+
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '/404' },
 ];
